431fe87d084eace5255d51f117b4bae53486eb58
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-29 11:34:23 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+until $(curl --output /dev/null --silent --head --fail http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT); do echo "Waiting for Elasticsearch to be online"; sleep 5; done
+sleep 3
+TEST=$(curl -I http://elasticsearch:9200/elastalert_status | head -n1)
+if [[ $TEST == *"404"* ]]; then
+  elastalert-create-index --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT --no-auth --no-ssl --index elastalert_status --url-prefix "" --old-index ""
+fi
