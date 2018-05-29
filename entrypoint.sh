b85de1999bcfa8e7650744259441e6ee7f24ef4e
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-29 12:20:23 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@ sleep 3
 TEST=$(curl -I http://elasticsearch:9200/elastalert_status | head -n1)
 if [[ $TEST == *"404"* ]]; then
   elastalert-create-index --host $ELASTICSEARCH_HOST --port $ELASTICSEARCH_PORT --no-auth --no-ssl --index elastalert_status --url-prefix "" --old-index ""
+else
+    echo "Elastalert index already exists in Elasticsearch."
 fi
+
+elastalert --config /etc/elastalert/config.yaml
